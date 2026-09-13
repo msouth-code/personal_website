9635c955a488ce43d63845fffbd5d8e83a944326
Miaya South <miayasouth2019@gmail.com>
2026-09-12 17:25:34 -0700

    created file for skills section

---
+++ src/Skills.jsx
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import './About.css';
+
+export default function Skills() {
+  return (
+    <Container>
+      <Row>
+        Skills go here
+      </Row>
+      </Container>
+  );
+}
