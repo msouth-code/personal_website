3ee8b9fdbe5822beaadff0b749c7dd1eb1b90759
Miaya South <miayasouth2019@gmail.com>
2026-09-12 17:37:42 -0700

    working on skills section styling

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,13 +1,24 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 import './About.css';
+import './Skills.css';
 
 export default function Skills() {
   return (
     <Container>
-      <Row>
-        Skills go here
-      </Row>
+      
+        <h2>Skills</h2>
+     
+      
+        <Stack direction="row" spacing={1} sx={{ maxWidth: 300, mx: 'auto' }}>
+            <Chip label="Python" variant="outlined" />
+            <Chip label="Java" variant="outlined" />
+            <Chip label="React" variant="outlined" />
+            <Chip label="HTML" variant="outlined" />
+        </Stack>
+      
       </Container>
   );
 }
