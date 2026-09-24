7865b9b176e621d45f6a3deaadd0b5059002607c
Miaya South <miayasouth2019@gmail.com>
2026-09-24 10:10:10 -0700

    finished addign some work experiences

--- src/Experiences.jsx
+++ src/Experiences.jsx
@@ -26,12 +26,21 @@ export default function Experiences() {
               </Typography>
               <Typography variant="body2">
                 <ul>
-                  <li>Worked to translate Ruby code for a Hulu API Downloader to Java and implemented updated features.</li>
-                  <li>Organized an entire project using low-level designs, sequence diagrams, and Ruby code 
-                    pseudo-code translation, both manually and by utilizing internal AI tools.</li>
-                  <li>Frequently presented project progress to the team during demo days, proactively met with my mentor 
-                    and manager for feedback, and studied the code of other engineers to improve my own and make it 
-                    more cohesive with the codebase.</li>
+                  <li>
+                    Worked to translate Ruby code for a Hulu API Downloader to
+                    Java and implemented updated features.
+                  </li>
+                  <li>
+                    Organized an entire project using low-level designs,
+                    sequence diagrams, and Ruby code pseudo-code translation,
+                    both manually and by utilizing internal AI tools.
+                  </li>
+                  <li>
+                    Frequently presented project progress to the team during
+                    demo days, proactively met with my mentor and manager for
+                    feedback, and studied the code of other engineers to improve
+                    my own and make it more cohesive with the codebase.
+                  </li>
                 </ul>
               </Typography>
             </CardContent>
@@ -54,7 +63,18 @@ export default function Experiences() {
               </Typography>
               <Typography variant="body2">
                 <ul>
-                  <li>wat i did</li>
+                  <li>
+                    Worked in React to develop a controlled component for main
+                    card feature on Discover page of application.
+                  </li>
+                  <li>
+                    Deployed several frontend changes on Discover page relating
+                    to dark mode.
+                  </li>
+                  <li>
+                    Actively engaged with every project presented to me over the
+                    course of 12 weeks.
+                  </li>
                 </ul>
               </Typography>
             </CardContent>
@@ -79,12 +99,16 @@ export default function Experiences() {
               </Typography>
               <Typography variant="body2">
                 <ul>
-                  <li>Guided and taught 2nd Grade students through assigned homework, organized supplementary materials, 
-                    and supervised activities such as field trips, physical education, and community activities.
+                  <li>
+                    Guided and taught 2nd Grade students through assigned
+                    homework, organized supplementary materials, and supervised
+                    activities such as field trips, physical education, and
+                    community activities.
                   </li>
                   <li>
-                    Supervised, supported, and bonded with over 170 middle school-aged girls over the entire duration 
-                    of summer-long sleepaway camp.
+                    Supervised, supported, and bonded with over 170 middle
+                    school-aged girls over the entire duration of summer-long
+                    sleepaway camp.
                   </li>
                 </ul>
               </Typography>
@@ -108,7 +132,52 @@ export default function Experiences() {
               </Typography>
               <Typography variant="body2">
                 <ul>
-                  <li>wat i did</li>
+                  <li>
+                    Guided and taught a class of 20 Kindergarten through 2nd
+                    grade students while simultaneously providing emotional
+                    support and daily structure.
+                  </li>
+                  <li>
+                    Developed and led 2 to 3 creative and engaging activities
+                    for students in Kindergarten through 5th grade per month.
+                  </li>
+                </ul>
+              </Typography>
+            </CardContent>
+          </Card>
+        </Box>
+        <Box className="experiences-cards">
+          <Card sx={{ borderRadius: "25px" }}>
+            <CardContent>
+              <Typography
+                gutterBottom
+                sx={{ color: "text.secondary", fontSize: 14 }}
+              >
+                September 2023 - June 2024
+              </Typography>
+              <Typography variant="h5" component="div">
+                HeyTutor
+              </Typography>
+              <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+                Lead Tutor
+              </Typography>
+              <Typography variant="body2">
+                <ul>
+                  <li>
+                    Tutored over 30 elementary students in English and
+                    Mathematics after school daily, and helped improve test
+                    scores by 15% overall in both subjects.
+                  </li>
+                  <li>
+                    Managed a group of after-school tutors and ensured they were
+                    administering tutoring material to their best ability and
+                    working to troubleshoot any problems that arose.
+                  </li>
+                  <li>
+                    Cultivated strong relationships with students to allow them
+                    to feel safe, respected, and excited to learn in the
+                    tutoring environment.
+                  </li>
                 </ul>
               </Typography>
             </CardContent>
