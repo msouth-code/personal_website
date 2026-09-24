a6b265edd5439a73fbea94ea5186c75d7b3f2f44
Miaya South <miayasouth2019@gmail.com>
2026-09-24 14:36:37 -0700

    working on styling of menu bar nd put contacts in menu bar

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,8 +1,9 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import "./Menu.css";
 
-function Menu({ aboutRef, skillsRef, experiencesRef }) {
+function Menu() {
 //   function scrollIntoView(type) {
 //     pageRefs.current[type].scrollIntoView({ behavior: "smooth" });
 //   }
@@ -11,15 +12,13 @@ function Menu({ aboutRef, skillsRef, experiencesRef }) {
     <Navbar expand="lg" className="bg-body-tertiary" sticky="top" 
     style={{backgroundColor: "#d980ff", border: "outset #ef78a6 2px", borderRadius: "45px"}}>
       <Container>
-        <Navbar.Brand >Miayunique South</Navbar.Brand>
+        <Navbar.Brand className="name-header">Miayunique South</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" class="ms-auto">
           <Nav className="me-auto">
-            <Nav.Link onClick={() => aboutRef.current?.scrollIntoView({ behavior: 'smooth' })} >About</Nav.Link>
-            <Nav.Link onClick={() => skillsRef.current?.scrollIntoView({ behavior: 'smooth' })} >Skills</Nav.Link>
-            <Nav.Link onClick={() => experiencesRef.current?.scrollIntoView({ behavior: 'smooth' })} >Experiences</Nav.Link>
+            <Nav.Link className="menu-item" href="https://www.linkedin.com/in/miayasouth" target="_blank">LinkedIn</Nav.Link>
+            <Nav.Link className="menu-item" href="mailto:miayasouth2019@gmail.com">Email</Nav.Link>
             {/* <Nav.Link href="/assets/South_Miayunique_Teaching_Resume_Sep2026.pdf" target="_blank">Resume</Nav.Link> */}
-            <Nav.Link mail-to >Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
